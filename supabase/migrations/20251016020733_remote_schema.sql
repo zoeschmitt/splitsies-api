9478d42ba1b0dc942c331543241b8e81bae06721
zoeschmitt <zoeschmitt@hotmail.com>
2025-10-15 21:11:20 -0500

    migrations repair

---
+++ supabase/migrations/20251016020733_remote_schema.sql
@@ -0,0 +1,44 @@
+revoke delete on table "public"."groups" from "anon";
+
+revoke insert on table "public"."groups" from "anon";
+
+revoke references on table "public"."groups" from "anon";
+
+revoke select on table "public"."groups" from "anon";
+
+revoke trigger on table "public"."groups" from "anon";
+
+revoke truncate on table "public"."groups" from "anon";
+
+revoke update on table "public"."groups" from "anon";
+
+revoke delete on table "public"."groups" from "authenticated";
+
+revoke insert on table "public"."groups" from "authenticated";
+
+revoke references on table "public"."groups" from "authenticated";
+
+revoke select on table "public"."groups" from "authenticated";
+
+revoke trigger on table "public"."groups" from "authenticated";
+
+revoke truncate on table "public"."groups" from "authenticated";
+
+revoke update on table "public"."groups" from "authenticated";
+
+revoke delete on table "public"."groups" from "service_role";
+
+revoke insert on table "public"."groups" from "service_role";
+
+revoke references on table "public"."groups" from "service_role";
+
+revoke select on table "public"."groups" from "service_role";
+
+revoke trigger on table "public"."groups" from "service_role";
+
+revoke truncate on table "public"."groups" from "service_role";
+
+revoke update on table "public"."groups" from "service_role";
+
+
+
