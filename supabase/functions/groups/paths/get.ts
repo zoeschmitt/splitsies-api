4202c08913ad01f8498fd4e6cb47bc8174d077d9
zoeschmitt <zoeschmitt@hotmail.com>
2025-11-06 20:30:50 -0600

    get groups

--- supabase/functions/groups/paths/get.ts
+++ supabase/functions/groups/paths/get.ts
@@ -1,29 +1,48 @@
 import { CompleteRequest } from "../../_shared/utils/requests.ts";
 import { ErrorCodes, apiError } from "../../_shared/utils/errors.ts";
 import { validate } from "../../_shared/utils/validate.ts";
-import { ObjectSchema, object } from "yup";
+import { ObjectSchema, object, string } from "yup";
 import { CORSResponse } from "../../_shared/utils/cors.ts";
+import { PostgrestError, SupabaseClient } from "@supabase/supabase-js";
 
-type Req = Record<string | number | symbol, unknown>;
-const schema: ObjectSchema<Req> = object();
+interface Req {
+  params: {
+    groupId: string;
+  };
+}
 
-const handler = async (req: CompleteRequest): Promise<Response> => {
+const schema: ObjectSchema<Req> = object({
+  params: object({
+    groupId: string().uuid().required(),
+  }),
+});
+
+const handler = async (
+  req: CompleteRequest,
+  sbClient: SupabaseClient
+): Promise<Response> => {
   try {
-    const { pathname } = new URL(req.url);
+    const { groupId } = req.params;
+    console.log("Fetching group with ID:", groupId);
+    const { data, error } = await sbClient
+      .from("groups")
+      .select("*")
+      .eq("id", groupId);
+    console.log("Fetched group data:", data);
+    if (error) throw error;
 
-    // if (!copilotId) {
-    //   throw new Error("Invalid copilotId");
-    // }
-
-    // if (copilot.userId && user?.id !== copilot.userId) {
-    //   return apiError(ErrorCodes.FORBIDDEN, {
-    //     error: "A different user owns this copilot.",
-    //   });
-    // }
-
-    return new CORSResponse();
+    return new CORSResponse(JSON.stringify({ group: data }), {
+      status: 200,
+    });
   } catch (error) {
     console.error("error:", error);
+
+    if (error instanceof PostgrestError && error?.code === "42501") {
+      return apiError(ErrorCodes.UNAUTHORIZED, {
+        error: "Insufficient permissions.",
+      });
+    }
+
     return apiError(ErrorCodes.SERVER_ERROR, error);
   }
 };
