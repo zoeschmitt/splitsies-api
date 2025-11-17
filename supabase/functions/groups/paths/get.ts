8fe9c4f890cefe3f77be2e460afb5bd479b9d4b6
zoeschmitt <zoeschmitt@hotmail.com>
2025-11-16 21:23:21 -0600

    get groups

--- supabase/functions/groups/paths/get.ts
+++ supabase/functions/groups/paths/get.ts
@@ -1,19 +1,19 @@
 import { CompleteRequest } from "../../_shared/utils/requests.ts";
-import { ErrorCodes, apiError } from "../../_shared/utils/errors.ts";
+import { withErrorHandling } from "../../_shared/utils/errors.ts";
 import { validate } from "../../_shared/utils/validate.ts";
 import { ObjectSchema, object, string } from "yup";
 import { CORSResponse } from "../../_shared/utils/cors.ts";
-import { PostgrestError, SupabaseClient } from "@supabase/supabase-js";
+import { SupabaseClient } from "@supabase/supabase-js";
 
 interface Req {
   params: {
-    groupId: string;
+    groupId?: string;
   };
 }
 
 const schema: ObjectSchema<Req> = object({
   params: object({
-    groupId: string().uuid().required(),
+    groupId: string().uuid().optional(),
   }),
 });
 
@@ -21,30 +21,17 @@ const handler = async (
   req: CompleteRequest,
   sbClient: SupabaseClient
 ): Promise<Response> => {
-  try {
-    const { groupId } = req.params;
-    console.log("Fetching group with ID:", groupId);
-    const { data, error } = await sbClient
-      .from("groups")
-      .select("*")
-      .eq("id", groupId);
-    console.log("Fetched group data:", data);
-    if (error) throw error;
+  const { groupId } = req.params;
 
-    return new CORSResponse(JSON.stringify({ group: data }), {
-      status: 200,
-    });
-  } catch (error) {
-    console.error("error:", error);
+  const query = sbClient.from("groups").select("*");
 
-    if (error instanceof PostgrestError && error?.code === "42501") {
-      return apiError(ErrorCodes.UNAUTHORIZED, {
-        error: "Insufficient permissions.",
-      });
-    }
+  const { data, error } = await (groupId ? query.eq("id", groupId) : query);
 
-    return apiError(ErrorCodes.SERVER_ERROR, error);
-  }
+  if (error) throw error;
+
+  return new CORSResponse(data, {
+    status: 200,
+  });
 };
 
-export const getGroups = validate(handler, schema);
+export const getGroups = validate(withErrorHandling(handler), schema);
